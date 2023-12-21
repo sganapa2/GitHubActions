ecbd5a65d5a6eacaf37e12c6ae785604635c39a1
sganapa2 <santoshganapa@gmail.com>
2023-12-21 14:56:18 +0530

    added health check actuator and modified rest endpoint

--- src/main/java/com/githubactions/learn/githubactionslearn/HelloGithubActionAnna.java
+++ src/main/java/com/githubactions/learn/githubactionslearn/HelloGithubActionAnna.java
@@ -1,17 +1,20 @@
 package com.githubactions.learn.githubactionslearn;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/entry")
+@RestController
+@Slf4j
 public class HelloGithubActionAnna {
 
-    @GetMapping("/welcome/{userName}")
-    public ResponseEntity<Object> sayHelloToUser(@PathVariable String userName) {
-        return ResponseEntity.ok("Hello " + userName);
+    @GetMapping("/welcome/{name}")
+    public ResponseEntity<Object> sayHelloToUser(@PathVariable String name) {
+        log.info("Request received in API");
+        return ResponseEntity.ok("Hello " + name);
     }
 
 }
